c8c882461b5d240badbae51402b42c53159c85f8
Jjman739 <Jjman739@gmail.com>
2024-01-26 13:41:33 -0500

    Add display colors

--- Assets/Scripts/Reservoir/PlateReservoirDisplay.cs
+++ Assets/Scripts/Reservoir/PlateReservoirDisplay.cs
@@ -7,6 +7,8 @@ public class PlateReservoirDisplay : MonoBehaviour
     [SerializeField] private GameObject plate;
     [SerializeField] private Transform stackParent;
     [SerializeField] private Vector3 offset;
+    [SerializeField] private Color perfectQualityColor;
+    [SerializeField] private Color minQualityColor;
 
     void Start()
     {
@@ -14,7 +16,7 @@ public class PlateReservoirDisplay : MonoBehaviour
         plate.SetActive(false);
     }
 
-    void RedoDisplay()
+    private void RedoDisplay()
     {
         foreach (Transform child in stackParent)
         {
@@ -26,8 +28,29 @@ public class PlateReservoirDisplay : MonoBehaviour
         {
             GameObject newPlate = Instantiate(plate, stackParent);
             newPlate.transform.localPosition = netOffset;
+            float quality = plateStats.GetDisplayQuality();
+            Color qualityColor = crossfadeColors(quality, perfectQualityColor, minQualityColor, minQualityColor);
+            newPlate.GetComponent<MeshRenderer>().material.color = qualityColor;
             newPlate.SetActive(true);
             netOffset += offset;
         }
+    }
+
+    private Color crossfadeColors(float quality, Color perfectColor, Color minColor, Color maxColor)
+    {
+        if (quality > 1)
+        {
+            minColor = maxColor;
+            quality = 2 - quality;
+        }
+
+        Vector3 perfectVector = new Vector3(perfectColor.r, perfectColor.g, perfectColor.b);
+        Vector3 minVector = new Vector3(minColor.r, minColor.g, minColor.b);
+        Vector3 crossfadeVector = Vector3.Lerp(minVector, perfectVector, quality);
+
+        float crossfadeR = crossfadeVector.x;
+        float crossfadeG = crossfadeVector.y;
+        float crossfadeB = crossfadeVector.z;
+        return new Color(crossfadeR, crossfadeG, crossfadeB);
     }
 }
