c82b801162cdb3910759ff0883ba32e57ea16a66
Jjman739 <Jjman739@gmail.com>
2024-01-26 14:07:37 -0500

    Add polymorphism to reservoir displays

--- Assets/Scripts/Reservoir/PlateReservoirDisplay.cs
+++ Assets/Scripts/Reservoir/PlateReservoirDisplay.cs
@@ -2,55 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlateReservoirDisplay : MonoBehaviour
+public class PlateReservoirDisplay : ReservoirDisplay
 {
-    [SerializeField] private GameObject plate;
-    [SerializeField] private Transform stackParent;
-    [SerializeField] private Vector3 offset;
-    [SerializeField] private Color perfectQualityColor;
-    [SerializeField] private Color minQualityColor;
+    private ReservoirStack<ReservoirPlate> reservoirStack;
 
     void Start()
     {
-        ReservoirManager.GetPlates().onReservoirUpdated.AddListener(RedoDisplay);
-        plate.SetActive(false);
+        reservoirStack = ReservoirManager.GetPlates();
+        reservoirStack.onReservoirUpdated.AddListener(RedoDisplay);
+        base.Start();
     }
 
-    private void RedoDisplay()
+    protected override List<ReservoirItem> getReservoirItems()
     {
-        foreach (Transform child in stackParent)
+        List<ReservoirItem> output = new List<ReservoirItem>();
+        foreach (ReservoirPlate plate in reservoirStack.GetAll())
         {
-            Destroy(child.gameObject);
+            output.Add(plate);
         }
-        List<ReservoirPlate> plates = ReservoirManager.GetPlates().GetAll();
-        Vector3 netOffset = new Vector3(0,0,0);
-        foreach (ReservoirPlate plateStats in plates)
-        {
-            GameObject newPlate = Instantiate(plate, stackParent);
-            newPlate.transform.localPosition = netOffset;
-            float quality = plateStats.GetDisplayQuality();
-            Color qualityColor = crossfadeColors(quality, perfectQualityColor, minQualityColor, minQualityColor);
-            newPlate.GetComponent<MeshRenderer>().material.color = qualityColor;
-            newPlate.SetActive(true);
-            netOffset += offset;
-        }
-    }
-
-    private Color crossfadeColors(float quality, Color perfectColor, Color minColor, Color maxColor)
-    {
-        if (quality > 1)
-        {
-            minColor = maxColor;
-            quality = 2 - quality;
-        }
-
-        Vector3 perfectVector = new Vector3(perfectColor.r, perfectColor.g, perfectColor.b);
-        Vector3 minVector = new Vector3(minColor.r, minColor.g, minColor.b);
-        Vector3 crossfadeVector = Vector3.Lerp(minVector, perfectVector, quality);
-
-        float crossfadeR = crossfadeVector.x;
-        float crossfadeG = crossfadeVector.y;
-        float crossfadeB = crossfadeVector.z;
-        return new Color(crossfadeR, crossfadeG, crossfadeB);
+        return output;
     }
 }
