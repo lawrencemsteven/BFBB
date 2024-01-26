6d2932a5ae45eb21c249a460b44850e2fbe58170
Jjman739 <Jjman739@gmail.com>
2024-01-26 13:18:31 -0500

    Add visible display

--- Assets/Scripts/Reservoir/PlateReservoirDisplay.cs
+++ Assets/Scripts/Reservoir/PlateReservoirDisplay.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class PlateReservoirDisplay : MonoBehaviour
 {
+    [SerializeField] private GameObject plate;
+    [SerializeField] private Transform stackParent;
+    [SerializeField] private Vector3 offset;
+
     void Start()
     {
         ReservoirManager.GetPlates().onReservoirUpdated.AddListener(RedoDisplay);
+        plate.SetActive(false);
     }
 
     void RedoDisplay()
     {
-        Debug.Log("I can't do that, Dave.");
+        foreach (Transform child in stackParent)
+        {
+            Destroy(child.gameObject);
+        }
+        List<ReservoirPlate> plates = ReservoirManager.GetPlates().GetAll();
+        Vector3 netOffset = new Vector3(0,0,0);
+        foreach (ReservoirPlate plateStats in plates)
+        {
+            GameObject newPlate = Instantiate(plate, stackParent);
+            newPlate.transform.localPosition = netOffset;
+            newPlate.SetActive(true);
+            netOffset += offset;
+        }
     }
 }
