230a798a709de1aa6194d5cb2c4c482f3a751546
Jjman739 <Jjman739@gmail.com>
2024-02-10 13:30:45 -0500

    Initial shape generator

---
+++ Assets/Scripts/Stations/PancakeStation/PancakeShapeGenerator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PancakeShapeGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject samplePoint;
+    [SerializeField] private LineRenderer lineRenderer;
+    public List<Vector2> coordinates;
+
+    void Start()
+    {
+        samplePoint.SetActive(false);
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.useWorldSpace = false;
+        GenerateShape();
+    }
+
+    public void GenerateShape()
+    {
+        lineRenderer.positionCount = coordinates.Count;
+        int i = 0;
+        foreach (Vector2 point in coordinates)
+        {
+            GameObject newPoint = GameObject.Instantiate(samplePoint, transform);
+            Vector3 position = new Vector3(point.x, 0, point.y);
+            newPoint.transform.localPosition = position;
+            lineRenderer.SetPosition(i, position);
+            i++;
+        }
+    }
+}
