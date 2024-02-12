eff26b9fc3626eeed0d8cc7667ae89bb53dfd447
Jjman739 <Jjman739@gmail.com>
2024-02-12 16:05:49 -0500

    Handle point collision

--- Assets/Scripts/CoordinateSystem/CoordinateCollider.cs
+++ Assets/Scripts/CoordinateSystem/CoordinateCollider.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class CoordinateCollider : MonoBehaviour
 {
+    public string targetTag;
     public int index;
 
     void OnCollisionEnter(Collision other)
     {
-
+        if (other.CompareTag(targetTag))
+        {
+            Station.HandlePointCollision(index);
+        }
     }
 }
