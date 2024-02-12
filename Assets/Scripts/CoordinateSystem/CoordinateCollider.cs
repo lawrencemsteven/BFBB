d60c333a5ede38ab189621423eb4b90a788e9063
Jjman739 <Jjman739@gmail.com>
2024-02-12 15:53:18 -0500

    Frameworks

---
+++ Assets/Scripts/CoordinateSystem/CoordinateCollider.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordinateCollider : MonoBehaviour
+{
+    public int index;
+
+    void OnCollisionEnter(Collision other)
+    {
+
+    }
+}
---
+++ Assets/Scripts/CoordinateSystem/CoordinateGenerator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordinateGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject pointPrefab;
+    [SerializeField] private float extent;
+    public List<Vector2> coordinates;
+
+    void Start()
+    {
+        GenerateShape();
+    }
+
+    public void GenerateShape()
+    {
+        int i = 0;
+        foreach (Vector2 point in coordinates)
+        {
+            GameObject newPoint = GameObject.Instantiate(samplePoint, transform);
+            Vector3 position = extent * new Vector3(point.x, 0, point.y);
+            newPoint.transform.localPosition = position;
+            newPoint.GetComponent<CoordinateCollider>().index = i;
+            i++;
+        }
+    }
+}
