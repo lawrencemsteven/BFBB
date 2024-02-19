cf71ccaa4bc5ccebacd85df047d347f650fa9f8a
Jjman739 <Jjman739@gmail.com>
2024-02-19 16:08:15 -0500

    Generate smudge coordinates

--- Assets/Scripts/Stations/DishStation/SmudgeCoordinateGenerator.cs
+++ Assets/Scripts/Stations/DishStation/SmudgeCoordinateGenerator.cs
@@ -19,10 +19,19 @@ public class SmudgeCoordinateGenerator : MonoBehaviour
     public void NewPlate()
     {
         int smudgeCount = Random.Range(minSmudges, maxSmudges + 1);
+        coordinateGenerator.coordinates = new List<Vector2>();
+
         for (int i = 0; i < smudgeCount; i++)
         {
             // Square stuff because otherwise they're going to be denser towards the middle.
             float radius = Mathf.Sqrt(Random.Range(0, Mathf.Pow(maxRadius, 2)));
+            float angle = Random.Range(0F, 360F);
+
+            Vector3 target3 = Quaternion.Euler(0, 0, angle) * Vector3.up;
+            Vector2 target = new Vector2(target3.x, target3.y);
+            coordinateGenerator.coordinates.Add(target);
         }
+
+        coordinateGenerator.GenerateShape();
     }
 }
