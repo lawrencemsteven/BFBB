e9d1c383452e7a03f58b76511519b1a0e15206da
ryondinkus <rjd323@drexel.edu>
2024-03-02 20:26:42 -0500

    suppress dishstation error when outside mainscene

--- Assets/Scripts/Stations/DishStation/DishStation.cs
+++ Assets/Scripts/Stations/DishStation/DishStation.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DishStation : Station
 {
@@ -19,6 +20,11 @@ public class DishStation : Station
     // Start is called before the first frame update
     void Start()
     {
+        if (SceneManager.GetActiveScene().name != "MainScene")
+        {
+            return;
+        }
+
         plate = transform.Find("Plate").gameObject;
         smudgesSpawnZone = transform.Find("Smudges").gameObject;
 
