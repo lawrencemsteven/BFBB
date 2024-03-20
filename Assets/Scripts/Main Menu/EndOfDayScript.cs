1e59b50f4c314f85989e62945c59b3b7a8477f9e
lawrencemsteven <lawrencemsteven@gmail.com>
2024-03-20 01:32:11 -0400

    Whoopsies

--- Assets/Scripts/Main Menu/EndOfDayScript.cs
+++ Assets/Scripts/Main Menu/EndOfDayScript.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using TMPro.Examples;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.UI;
 
